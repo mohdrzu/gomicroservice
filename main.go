2c82653d2e297209ea13744e459308156ed4235e
Muhammad Reza Zulman <m.rezazulman@gmail.com>
2024-01-22 12:06:51 +0700

    read request and write response

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"io"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World")
+		data, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Oops", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(w, "Hello %s", data)
 	})
 
 	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Goodbye World")
+		data, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Oops", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(w, "Goodbye %s", data)
 	})
 
 	
