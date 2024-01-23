ad90b340a1f5502143ecfe57daba60d63d4955ce
Muhammad Reza Zulman <m.rezazulman@gmail.com>
2024-01-23 10:19:57 +0700

    update product

--- handlers/product.go
+++ handlers/product.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/mohdrzu/gomicroservice/models"
 )
@@ -26,6 +28,31 @@ func(p *Product) ServeHTTP(w http.ResponseWriter, r *http.Request){
 		return
 	 }
 
+	 if r.Method == http.MethodPut {
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			http.Error(w, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		if len(g[0]) != 2 {
+			http.Error(w, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			http.Error(w, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		p.updateProduct(id, w, r)
+		
+		return
+	 }
+
 	 w.WriteHeader(http.StatusMethodNotAllowed)
 }
 
@@ -46,4 +73,24 @@ func (p *Product) addProduct(w http.ResponseWriter, r *http.Request) {
 
 	models.AddProduct(prod)
 
+}
+
+func (p *Product) updateProduct(id int, w http.ResponseWriter, r *http.Request){
+	prod := &models.Product{}
+	err := prod.FromJson(r.Body)
+	if err != nil {
+		http.Error(w, "unable to unmarshall json", http.StatusBadRequest)
+	}
+
+	err = models.UpdateProduct(id, prod)
+	if err == models.ErrProductNotFound {
+		http.Error(w, "product not found", http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, "product not found", http.StatusNotFound)
+		return	
+	}
+	
+	models.UpdateProduct(id, prod)
 }
